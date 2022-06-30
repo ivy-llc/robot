593bf2ec9b5518287045e85e602e5712839fb2b2
Jatin Prakash <47978882+bicycleman15@users.noreply.github.com>
2022-06-30 08:12:42 +0000

    update docker image name

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-docker run --rm -it -v "$(pwd)":/ivy_robot unifyai/ivy-robot:latest python3 -m pytest ivy_robot_tests/
+docker run --rm -it -v "$(pwd)":/ivy_robot unifyai/robot:latest python3 -m pytest ivy_robot_tests/
