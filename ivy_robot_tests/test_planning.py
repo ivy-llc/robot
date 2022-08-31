abb1c70bc9fba467171bbff9f222c14cfe850bec
JG <67888047+juliagsy@users.noreply.github.com>
2022-08-31 04:05:00 +0000

    fix test_spline_interpolation tests

--- ivy_robot_tests/test_planning.py
+++ ivy_robot_tests/test_planning.py
@@ -30,6 +30,6 @@ td = SplineTestData()
 def test_spline_interpolation(dev_str, fw):
     ivy.set_backend(fw)
     assert np.allclose(
-        ivy_robot.planning.sample_spline_path(td.train_points_3d, td.train_values_3d,
-             td.query_points_3d), td.query_values_3d, atol=1e-2)
+        ivy_robot.planning.sample_spline_path(ivy.array(td.train_points_3d), ivy.array(td.train_values_3d),
+             ivy.array(td.query_points_3d)), td.query_values_3d, atol=1e-2)
     ivy.unset_backend()
